9f704a5457488bf0c2d87eafa01c58c22eef08ee
Ibz Odumade <IbzOdus@gmail.com>
2021-04-23 07:42:47 +0100

    Use standard ISO format for date, add more job examples and make init script more efficient

--- db_service/initialise_data.py
+++ db_service/initialise_data.py
@@ -7,19 +7,20 @@ import json
 with open(f"{os.pardir}{os.sep}mock_jobs.json", "r") as reader:
     data = json.loads(reader.read())
 
-for record in data:
-    entity = Job(
-        id=record["id"],
-        title=record["title"],
-        salary=record["salary"],
-        start_date=datetime.strptime(record["startDate"], "%d/%m/%Y"),
-        location=record["location"],
-        company=record["company"],
-        sector=record["sector"],
-        description=record["description"],
-        created=datetime.utcnow(),
-    )
+with (app.app_context()):
+    for record in data:
+        entity = Job(
+            id=record["id"],
+            title=record["title"],
+            salary=record["salary"],
+            start_date=datetime.strptime(record["startDate"], "%Y-%m-%d"),
+            location=record["location"],
+            company=record["company"],
+            sector=record["sector"],
+            description=record["description"],
+            created=datetime.strptime(record["created"], "%Y-%m-%d"),
+        )
 
-    with (app.app_context()):
         db.session.add(entity)
-        db.session.commit()
+
+    db.session.commit()
