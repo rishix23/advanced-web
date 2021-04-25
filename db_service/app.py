758d7006c28d491584f5ed7ccdd17861fe6506d6
Ibz Odumade <IbzOdus@gmail.com>
2021-04-25 21:46:06 +0100

    Move db into db service and create basic endpoints for each model

--- db_service/app.py
+++ db_service/app.py
@@ -1,9 +1,9 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy, request
+from flask import Flask, Response
+from flask_sqlalchemy import SQLAlchemy
 from models import metadata, Job, User, Application, Employer
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///../app.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
 db = SQLAlchemy(metadata=metadata)
 db.init_app(app)
 
@@ -11,9 +11,28 @@ with app.app_context():
     db.create_all()
 
 
-@app.route("/job", methods=["GET", "POST", "PUT", "PATCH", "DELETE"])
-def index():
-    return "Get"
+@app.route("/jobs", methods=["GET", "POST", "PUT", "PATCH", "DELETE"])
+def handle_jobs():
+    jobs = db.session.query(Job).all()
+    return str(jobs)
+
+
+@app.route("/users", methods=["GET", "POST", "PUT", "PATCH", "DELETE"])
+def handle_users():
+    users = db.session.query(User).all()
+    return str(users)
+
+
+@app.route("/applications", methods=["GET", "POST", "PUT", "PATCH", "DELETE"])
+def handle_applications():
+    applications = db.session.query(Application).all()
+    return str(applications)
+
+
+@app.route("/employers", methods=["GET", "POST", "PUT", "PATCH", "DELETE"])
+def handle_employers():
+    employers = db.session.query(Employer).all()
+    return str(employers)
 
 
 if __name__ == "__main__":
