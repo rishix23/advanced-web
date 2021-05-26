51eaaccb737441e219a1eceb8ee78111b6e07550
Sam Foster <sf00563@surrey.ac.uk>
2021-05-27 00:36:03 +0100

    changed layout of my jobs page to include options within the displayed job

--- frontend-react/src/components/myjobs/Myjobs.js
+++ frontend-react/src/components/myjobs/Myjobs.js
@@ -32,27 +32,23 @@ function Myjobs() {
     <div className='myjobs-wrapper-main'>
       <h1 className='myjobs-title'>1 Active job(s)</h1>
       <div className='myjobs-wrapper'>
-        <div className='myjobs-individual'>
-          <div className='myjobs-individual-info'>
-            <div className='myjobs-individual-applicants'>
-              <p className='myjobs-individual-title'>{userJobs.title}</p>
-              <strong><Link to={`/myjobs/${userJobs.id}`}>View all applicants</Link></strong>
-            </div>
-            <p className='myjobs-individual-posted'>Date posted: {userJobs.created}</p>
-            <p>Location: {userJobs.location}</p>
-            <p>Start date: {userJobs.start_date}</p>
-            <p>Salary: {userJobs.salary}</p>
-            <p className='myjobs-individual-description-title'>Description:</p>
-            <p className='myjobs-individual-description'>{userJobs.description}</p>
-          </div>
-          <div className='myjobs-individual-options'>
-            <div className='myjobs-individual-options-icons'>
+        <div className='myjobs-individual-info'>
+          <div className='myjobs-individual-applicants'>
+            <p className='myjobs-individual-title'>{userJobs.title}</p>
+            <div className='myjobs-individual-applicants-wrapper'>
+              <strong><Link className="applicants-link" to={`/myjobs/${userJobs.id}`}>View all applicants</Link></strong>
               <Link to="/">
                 <img src={editicon} alt="" />
               </Link>
-              <button className="myjobs-individual-options-icons-button"><img src={deleteicon} alt="" onClick={myFunction} /></button>
+              <button className="myjobs-individual-applicants-icons-button"><img src={deleteicon} alt="" onClick={myFunction} /></button>
             </div>
           </div>
+          <p>Date posted: {userJobs.created}</p>
+          <p>Location: {userJobs.location}</p>
+          <p>Start date: {userJobs.start_date}</p>
+          <p>Salary: {userJobs.salary}</p>
+          <p className='myjobs-individual-description-title'>Description:</p>
+          <p className='myjobs-individual-description'>{userJobs.description}</p>
         </div>
       </div>
     </div>
