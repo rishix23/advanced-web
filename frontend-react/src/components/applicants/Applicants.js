91b0d29a090bb2eebb393cb9a0c8a991339422ea
Sam Foster <sf00563@surrey.ac.uk>
2021-05-27 01:23:28 +0100

    added in table of applicants

--- frontend-react/src/components/applicants/Applicants.js
+++ frontend-react/src/components/applicants/Applicants.js
@@ -3,8 +3,32 @@ import './Applicants.css';
 
 function Applicants() {
   return (
-    <div>
-      <h1>Applicants</h1>
+    <div className='applicants-wrapper-main'>
+      <h1 className='applicants-title'>Applicants</h1>
+      <div className='grid-wrapper'>
+        <div className='grid'>
+          <span>
+            <strong>Applicant Id</strong>
+          </span>
+          <span>
+            <strong>FullName</strong>
+          </span>
+          <span>
+            <strong>Phone</strong>
+          </span>
+          <span>
+            <strong>Email</strong>
+          </span>
+          <span>
+            <strong>CV</strong>
+          </span>
+          <span>1</span>
+          <span>Bill Bryson</span>
+          <span>07922429723</span>
+          <span>bbryson@gmail.com</span>
+          <span>Download</span>
+        </div>
+      </div>
     </div>
   );
 }
