a28c75b09f7665ae1b8b0552a97fe5c3d651787e
Rishi Patel <rishipatel@Rishis-MacBook-Pro.local>
2021-05-27 13:45:05 +0100

    updating cv creator service

--- cv-creator-service/app.py
+++ cv-creator-service/app.py
@@ -1,26 +1,37 @@
-from flask import Flask, request, render_template, Response, make_response, jsonify
+from flask import Flask, request, render_template, Response, make_response, jsonify, send_file
 from datetime import datetime
-import cv_creator as cv 
+from docx import Document
+import cv_creator as cv
+import io
+from io import BytesIO
 
 app = Flask(__name__)
 
-# @app.route("/", methods=["POST", "GET"])
-# def index():
-#     return render_template("index.html")
+@app.route("/", methods=["POST", "GET"])
+def index():
+    return render_template("index.html")
 
 @app.route("/CVTemplate1", methods=['POST', "OPTIONS"])
 def createTemplate1():
-    if request.method == "OPTIONS": # CORS preflight
+
+     # CORS preflight
+    if request.method == "OPTIONS":
         return _build_cors_prelight_response()
-    elif request.method == "POST": # The actual request following the prefligh
-        info = jsonify({"order_id": 123, "status": "shipped"})
-        #resp = Response()
-        #resp.headers['Access-Control-Allow-Origin'] = 'http://localhost:3000'
-        #response = request.get_json()
-        #print("im here")
-        #print(response)
-        #cv.template1()
-        return _corsify_actual_response(info)
+    # The actual request following the preflight
+    elif request.method == "POST": 
+        
+        json_object = request.json
+        print(json_object)
+ 
+        document = cv.template1(json_object)
+        f = io.BytesIO()
+        document.save(f)
+        length = f.tell()
+        f.seek(0)
+
+        response = send_file(f, as_attachment=True, attachment_filename='cv.doc')
+
+        return _corsify_actual_response(response)
  
 def _build_cors_prelight_response():
     response = make_response()
@@ -35,3 +46,9 @@ def _corsify_actual_response(response):
 
 if __name__ == "__main__":
     app.run()
+
+# git branch "name" / 
+# git checkout "name"/ 
+# git add ./ 
+# git commit -m/ 
+# git push "name"
--- cv-creator-service/cv_creator.py
+++ cv-creator-service/cv_creator.py
@@ -5,7 +5,7 @@ from docx.enum.style import WD_STYLE_TYPE
 from docx.shared import Inches, Cm
 from docx.shared import Pt
 
-def template1():
+def template1(json_object):
     # initiate a new word document with custom margins
     document = Document()
     sections = document.sections
@@ -15,11 +15,6 @@ def template1():
         section.left_margin = Cm(1.5)
         section.right_margin = Cm(1.5)
 
-    # style = document.styles['Normal']
-    # font = style.font
-    # font.name = 'Calibri (Headings)'
-    # font.size = Pt(12)
-
     obj_styles = document.styles
     obj_charstyle = obj_styles.add_style('CommentsStyle', WD_STYLE_TYPE.CHARACTER)
     obj_font = obj_charstyle.font
@@ -27,9 +22,10 @@ def template1():
     obj_font.name = 'Times New Roman'
 
     # add user info to document on upper right-hand corner
-    address = "Guildford, UK"
-    telephone = "07950974823"
-    email = "rp00463@surrey.ac.uk"
+    name = json_object['fullname']
+    address = json_object['address']
+    telephone = json_object['telephone']
+    email = json_object['email']
 
     paragraph = document.add_paragraph()
     paragraph.add_run('Location: ').bold = True
@@ -45,21 +41,6 @@ def template1():
     street_address_p_format = paragraph.paragraph_format
     street_address_p_format.alignment = WD_ALIGN_PARAGRAPH.RIGHT
 
-    # set all variables to inputs from front-end
-    name = "Rishi Patel"
-    professional_profile = "I love working in teams and I am a great problem solver."
-
-    education = "University of Surrey"
-    education_date = "2017-2021"
-
-    work_experience = "IBM"
-    work_experience_date = "2019-2020"
-
-    volunteering_experience = "Food bank"
-    volunteering_experience_date = "2012-2016"
-    
-    skills = ["tennis, C#"]
-
     certificates_achievements = ""
 
     # add name to document
@@ -67,55 +48,70 @@ def template1():
 
     # profile section
     document.add_heading('Professional Profile', level=1)
-    professional_profile_p = document.add_paragraph(professional_profile)
+    professional_profile_p = document.add_paragraph(json_object['strengths'])
 
     # education section
     document.add_heading('Education', level=1)
-    education_p = document.add_paragraph(style='List Bullet')
+    education_items = int(len(json_object['education']))
 
-    # add for loop here
-
-    education_p.add_run(education, style = 'CommentsStyle').bold = True
-    run = education_p.add_run()
-    run.add_break()
-    education_p.add_run(education_date)
+    # loop through each item for education
+    for i in range(education_items):
+        school = json_object['education'][i]['school']
+        period = json_object['education'][i]['period']
+        description = json_object['education'][i]['description']
+        education_p = document.add_paragraph(style='List Bullet')
+        education_p.add_run(school + " (" + period + ")").bold = True
+        run = education_p.add_run()
+        run.add_break()
+        education_p.add_run(description)
 
     # work experience section
-    document.add_heading('Work Expereince', level=1)
-    work_experience_p = document.add_paragraph(style='List Bullet')
-    # add for loop here
+    document.add_heading('Work Experience', level=1)
+    work_experience_items = int(len(json_object['workexp']))
+    
+    # loop through each item for work experience 
+    for i in range(work_experience_items):
+        company = json_object['workexp'][i]['company']
+        period = json_object['workexp'][i]['period']
+        description = json_object['workexp'][i]['description']
+        work_experience_p = document.add_paragraph(style='List Bullet')
+        work_experience_p.add_run(company + " (" + period + ")").bold = True
+        run = work_experience_p.add_run()
+        run.add_break()
+        work_experience_p.add_run(description)
 
-    work_experience_p.add_run(work_experience).bold = True
-    run = work_experience_p.add_run()
-    run.add_break()
-    work_experience_p.add_run(work_experience_date)
-
-    # volunteering experience section
+        # volunteering section
     document.add_heading('Volunteering', level=1)
-    volunteering_p = document.add_paragraph(style='List Bullet')
-    # add for loop here
-    volunteering_p.add_run(volunteering_experience).bold = True
-    run = volunteering_p.add_run()
-    run.add_break()
-    volunteering_p.add_run(volunteering_experience_date)
+    volunteering_items = int(len(json_object['volunteering']))
 
-    # skills section
+    # loop through each item for volunteering experience 
+    for i in range(volunteering_items):
+        organisation = json_object['volunteering'][i]['org']
+        period = json_object['volunteering'][i]['period']
+        description = json_object['volunteering'][i]['description']
+        volunteering_p = document.add_paragraph(style='List Bullet')
+        volunteering_p.add_run(organisation + " (" + period + ")").bold = True
+        run = volunteering_p.add_run()
+        run.add_break()
+        volunteering_p.add_run(description)
+        
+    # add skills section
     document.add_heading('Skills', level=1)
     skills_p = document.add_paragraph(style='List Bullet')
-    for skill in skills:
-        skills_p.add_run(skill)
+    skills_p.add_run(json_object['skills'])
 
-    # Achievments/Accolades/Certificates section
+    # add achievments/accolades/certificates section
     document.add_heading('Achievments/Accolades/Certificates', level=1)
     certificates_achievements_p = document.add_paragraph(style='List Bullet')
-    # add for loop here
-    certificates_achievements_p.add_run(certificates_achievements).bold = True
+    certificates_achievements_p.add_run(json_object['profqual']['qual'] + " (" + json_object['profqual']['date'] + ")").bold = True
 
-    document.add_picture('butterfly.png', width=Inches(3))
+    # document.add_picture('butterfly.png', width=Inches(3))
 
     # # document.add_page_break()
 
-    document.save('demo.docx')
+    # document.save('demo.docx')
 
-if __name__ == "__main__":
-    template1()
+    return document
+
+# if __name__ == "__main__":
+#     template1()
