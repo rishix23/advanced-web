9bbe6f44930de93656ecaff37a6dadd7e450b77a
Sam Foster <sf00563@surrey.ac.uk>
2021-05-29 18:56:44 +0100

    connected the delet button to the relevant end point in job service micro service

--- frontend-react/src/components/myjobs/Myjobs.js
+++ frontend-react/src/components/myjobs/Myjobs.js
@@ -15,23 +15,23 @@ function Myjobs() {
   const { authTokens } = useAuth();
 
   const fetchUserJobs = async () => {
-    const employerid = authTokens.slice(1, -1);
-    const data = await fetch(`http://localhost:5000/?employerId=${employerid}`);
+    const data = await fetch(`http://localhost:5000/?employerId=${authTokens}`);
     const userJobs = await data.json();
     setUserJobs(userJobs);
     console.log(userJobs);
   }
 
-  const myFunction = () => {
-    console.log("you clicked me")
+  const deleteJob = (jobId) => {
+    fetch(`http://localhost:5000/${jobId}`, { method: 'DELETE' })
+      .then(() => setDeleteSuccess(true));
   }
 
-  //remember to change to array
   const [userJobs, setUserJobs] = useState([]);
+  const [deleteSuccess, setDeleteSuccess] = useState(false);
 
   return (
     <div className='myjobs-wrapper-main'>
-      <h1 className='myjobs-title'>1 Active job(s)</h1>
+      <h1 className='myjobs-title'>{userJobs.length} Active job(s)</h1>
       <div className='myjobs-wrapper'>
         {userJobs.map(job => (
           <div key={job.id} className='myjobs-individual-info'>
@@ -42,7 +42,7 @@ function Myjobs() {
                 <Link to={`/myjobs/${job.id}/edit`}>
                   <img src={editicon} alt="" />
                 </Link>
-                <button className="myjobs-individual-applicants-icons-button"><img src={deleteicon} alt="" onClick={myFunction} /></button>
+                <button className="myjobs-individual-applicants-icons-button" onClick={() => deleteJob(job.id)}><img src={deleteicon} alt="" /></button>
               </div>
             </div>
             <p>Date posted: {job.created}</p>
@@ -51,6 +51,7 @@ function Myjobs() {
             <p>Salary: jobs salary</p>
             <p className='myjobs-individual-description-title'>Description:</p>
             <p className='myjobs-individual-description'>{job.description}</p>
+            {deleteSuccess && <h4 className='delete-msg'>Job successfully deleted!</h4>}
           </div>
         ))}
       </div>
