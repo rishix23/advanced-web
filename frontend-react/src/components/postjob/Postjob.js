4627438cf1d56adac1f1c79b114147618c1dd69c
Sam Foster <sf00563@surrey.ac.uk>
2021-05-29 14:19:44 +0100

    added start date field to form for post a job page

--- frontend-react/src/components/postjob/Postjob.js
+++ frontend-react/src/components/postjob/Postjob.js
@@ -6,12 +6,18 @@ import './Postjob.css'
 function PostJob() {
   const { register, handleSubmit, formState: { errors } } = useForm();
   const { authTokens } = useAuth();
-  const [isJobPosted, setJobPosted] = useState(false);
 
-  const onSubmit = (data) => {
-    const dataWithUser = { ...authTokens, ...data };
-    console.log(dataWithUser);
-    setJobPosted(true);
+  const onSubmit = (formData) => {
+    formData.employerId = JSON.parse(authTokens)
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(formData),
+    };
+    fetch('http://localhost:5000/', requestOptions)
+      .then(response => response.json())
+      .then(data => console.log(data));
+
   }
 
   return (
@@ -19,10 +25,12 @@ function PostJob() {
       <h3 className='postjob-title'>Post a job</h3>
       <div className='postjob-wrapper'>
         <form onSubmit={handleSubmit(onSubmit)}>
-          <input type="text" placeholder="Job title" {...register('jobtitle', { required: "This is required!" })} />
-          {errors.jobtitle && <p>{errors.jobtitle.message}</p>}
+          <input type="text" placeholder="Job title" {...register('title', { required: "This is required!" })} />
+          {errors.title && <p>{errors.title.message}</p>}
           <input type="text" placeholder="Salary" {...register('salary', { required: "This is required!" })} />
           {errors.salary && <p>{errors.salary.message}</p>}
+          <input type="text" placeholder="Start date" {...register('startDate', { required: "This is required!" })} />
+          {errors.startDate && <p>{errors.startDate.message}</p>}
           <input type="text" placeholder="Location" {...register('location', { required: "This is required!" })} />
           {errors.location && <p>{errors.location.message}</p>}
           <input type="text" placeholder="Your company" {...register('company', { required: "This is required!" })} />
@@ -42,7 +50,7 @@ function PostJob() {
           {errors.jobdescription && <p>{errors.jobdescription.message}</p>}
           <input type="submit" />
         </form>
-        {isJobPosted && <h4 className='postjob-msg'>Your job has been successfully posted!</h4>}
+        <h4 className='postjob-msg'>Your job has been successfully posted!</h4>
       </div>
     </div>
   );
